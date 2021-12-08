6c64c6c781be797fbcffcc49ec5e6a1dd34cf1b0
Rashin Aeini <aeini.rashin@gmail.com>
2021-12-08 18:55:42 +0330

    Complete ajax for PeopleService

---
+++ PeopleAssignment/Controllers/AjaxController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleAssignment.Models;
+using PeopleAssignment.Models.Services;
+
+namespace PeopleAssignment.Controllers
+{
+    public class AjaxController : Controller
+    {
+        private IPeopleService Service { get; }
+
+        public AjaxController()
+        {
+            Service = new PeopleService();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Fetch()
+        {
+            return PartialView(Service.All());
+        }
+
+        [HttpPost]
+        public IActionResult Details(int id)
+        {
+            Person person = Service.FindById(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView(person);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Person person = Service.FindById(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            Service.Remove(id);
+
+            return Ok();
+        }
+    }
+}
