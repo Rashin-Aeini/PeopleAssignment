279633637c758fd0435d8ebc4b05b21551ad3237
Rashin Aeini <aeini.rashin@gmail.com>
2021-12-12 15:25:37 +0330

    Migration for Relation between City and Person

---
+++ PeopleAssignment/Migrations/20211212110920_RelationCityAndPerson.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PeopleAssignment.Migrations
+{
+    public partial class RelationCityAndPerson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Persons");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CityId",
+                table: "Persons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Persons_CityId",
+                table: "Persons",
+                column: "CityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Persons_Cities_CityId",
+                table: "Persons",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Persons_Cities_CityId",
+                table: "Persons");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Persons_CityId",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Persons");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Persons",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
